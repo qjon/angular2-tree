6ab195da36f753914327127fc8f1d4271c8f6220
Rafał Ignaszewski <rafal@ignaszewski.pl>
2018-09-19 17:30:57 +0200

    Auto-generated commit

--- src/service/nodesDispatcher.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { INodeService, NodeService } from './node.service';
-export declare class NodeDispatcherService {
-    private nodeService;
-    constructor(nodeService: NodeService[]);
-    get(key: string): INodeService;
-}
